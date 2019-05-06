3927cb3fdc5fd2309bfaf33de927413ca9925cd4
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-05-06 07:07:45 -0300

    Inclusão de BackEnd SpringBoot

---
+++ rodrigobranas/angularjs/backEnd/contatospi/src/main/java/com/roberto/listatelefonica/api/ContatospiApplication.java
@@ -0,0 +1,14 @@
+package com.roberto.listatelefonica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatospiApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ContatospiApplication.class, args);
+	}
+
+}
