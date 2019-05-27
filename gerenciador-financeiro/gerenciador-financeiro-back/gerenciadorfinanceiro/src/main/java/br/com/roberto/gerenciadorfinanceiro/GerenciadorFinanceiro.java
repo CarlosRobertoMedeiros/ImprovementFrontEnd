6483b499e258173337d59717d65466f11ebec45c
Carlos Roberto Medeiros de Lima <carlosmedeiroslima@gmail.com>
2019-05-27 20:25:30 -0300

    Gereniador Financeiro

---
+++ gerenciador-financeiro/gerenciador-financeiro-back/gerenciadorfinanceiro/src/main/java/br/com/roberto/gerenciadorfinanceiro/GerenciadorFinanceiro.java
@@ -0,0 +1,15 @@
+package br.com.roberto.gerenciadorfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorFinanceiro {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorFinanceiro.class, args);
+	}
+
+}
+
+//Continuar do video 3.10 - Minuto 13
