192e23ed2b1f8cf23abf1ed8da2a3d1207845a40
Carlos Roberto Medeiros de Lima <carlosmedeiroslima@gmail.com>
2019-06-14 18:39:15 -0300

    Implementando Segurança Basic com Spring Security

--- gerenciador-financeiro/gerenciador-financeiro-back/gerenciadorfinanceiro/src/main/java/br/com/roberto/gerenciadorfinanceiro/GerenciadorFinanceiro.java
+++ gerenciador-financeiro/gerenciador-financeiro-back/gerenciadorfinanceiro/src/main/java/br/com/roberto/gerenciadorfinanceiro/GerenciadorFinanceiro.java
@@ -13,19 +13,30 @@ public class GerenciadorFinanceiro {
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorFinanceiro.class, args);
 	}
-	
-	//Ajusta o TimeZone para UTC
-	//Todos os campos LocalDate ou LocalDateTime vão ser modificados
-	@PostConstruct
-	  void started() {
-	    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	  }
-	 
 
+	// Ajusta o TimeZone para UTC
+	// Todos os campos LocalDate ou LocalDateTime vão ser modificados
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
 
-//Verificar a dúvida no ví 2deo da aula 3.10
-//Continuar do 5.3. Desafio: Cadastrando o primeiro lançamento
-// O código está implementado, porém não estou sabendo fazer o post corretamente
-// Continuar no 5.7 --Pesquisa de Lancamento com MetaModel JPA --Continuar o código a partir da aula 5.7
-//                  --Continuar a Partir dos 10 minutos, o software está dando erro
+// Verificar a dúvida no vídeo da aula 3.10
+// Continuar do 5.3. Desafio: Cadastrando o primeiro lançamento
+// O Código está implementado, porém não estou sabendo fazer o post corretamente
+// Continuar no 6.3. Implementando segurança com OAuth 2 e Password Flow
+// XXXXXXXXXXXXXXXX -- Continuar a Partir dos 10 minutos, o software está dando erro
+// Utilizando a Autenticação
+    //Senha com Basic
+
+//Utilizando OAuth2
+    // user
+	// client
+    // autorization server
+	//	    - Envia o Token para o client
+
+    // resource server
+    // 		- Guarda as funcionalidades do sistema(lancamentos,categorias,pessoas)
+// Links para leitura: https://www.digitalocean.com/community/tutorials?q=Oauth2
+//
